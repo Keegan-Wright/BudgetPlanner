722203b2452b9377ec2568b9a175b279d11b3fe9
Keegan Wright <Keeganwright12@gmail.com>
2024-08-01 21:04:43 +0100

    Initial commit

---
+++ BudgetPlanner/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BudgetPlanner.ViewModels
+{
+    public partial class MainViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        private string _greeting = "Welcome to Avalonia!";
+    }
+}
---
+++ BudgetPlanner/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace BudgetPlanner.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}

12fae415e525cbf289acc32550e2e4946713ebf8
Keegan Wright <Keeganwright12@gmail.com>
2024-09-22 20:04:00 +0100

    updates offline networking handler but only works for windows at the moment

--- BudgetPlanner.States/ApplicationState.cs
+++ BudgetPlanner.States/ApplicationState.cs
@@ -4,6 +4,25 @@ namespace BudgetPlanner.States
 {
     public class ApplicationState
     {
-        public static bool HasInternetConnection { get; set; } = NetworkInterface.GetIsNetworkAvailable();
+        public static bool HasInternetConnection => CheckConnection();
+
+        private static bool CheckConnection()
+        {
+            var all = System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var item in all)
+            {
+                if (item.NetworkInterfaceType == NetworkInterfaceType.Loopback)
+                    continue;
+                if (item.Name.ToLower().Contains("virtual") || item.Description.ToLower().Contains("virtual"))
+                    continue; //Exclude virtual networks set up by VMWare and others
+                if (item.OperationalStatus == OperationalStatus.Up)
+                {
+                    var b = item.GetPhysicalAddress();
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }

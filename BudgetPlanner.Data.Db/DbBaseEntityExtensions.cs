a9aa79f4085643fc4f6ea96c83f349f8504d83ad
Keegan Wright <Keeganwright12@gmail.com>
2024-10-02 22:20:33 +0100

    DB paging helper

---
+++ BudgetPlanner.Data.Db/DbBaseEntityExtensions.cs
@@ -0,0 +1,35 @@
+﻿using BudgetPlanner.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetPlanner.Data.Db
+{
+    public static class DbBaseEntityExtensions
+    {
+        public static async IAsyncEnumerable<TEntityType> GetPagedEntitiesAsync<TEntityType>(this IQueryable<TEntityType> entities, int pageSize, int currentPage = 0, int? maxPages = null)
+            where TEntityType : BaseEntity
+        {
+            var totalCount = await entities.CountAsync();
+            var pagesToRun = totalCount / (currentPage + 1 * pageSize);
+
+            while (pagesToRun > currentPage + 1)
+            {
+                if (currentPage >= maxPages)
+                {
+                    yield break;
+                }
+
+                await foreach (var entity in entities
+                                            .Skip(currentPage * pageSize)
+                                            .Take(pageSize)
+                                            .AsAsyncEnumerable())
+                {
+                    yield return entity;
+                }
+                currentPage++;
+
+            }
+
+        }
+    }
+
+}

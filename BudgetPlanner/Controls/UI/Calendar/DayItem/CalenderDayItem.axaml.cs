072873c4286f228ca579bd27758e354d9d43fc58
Keegan Wright <Keeganwright12@gmail.com>
2024-12-03 08:47:54 +0000

    Calender days rendering

--- BudgetPlanner/Controls/UI/Calendar/DayItem/CalenderDayItem.axaml.cs
+++ BudgetPlanner/Controls/UI/Calendar/DayItem/CalenderDayItem.axaml.cs
@@ -1,9 +1,22 @@
+using System.Collections.Generic;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using BudgetPlanner.ViewModels;
 
 namespace BudgetPlanner.Controls;
 
 public class CalenderDayItem : TemplatedControl
 {
+    public static readonly DirectProperty<CalenderDayItem, CalendarItemViewModel> DayProperty =
+        AvaloniaProperty.RegisterDirect<CalenderDayItem, CalendarItemViewModel>(nameof(Day),
+            p => p.Day, (p, v) => p.Day = v);
+    
+    private CalendarItemViewModel _day = new CalendarItemViewModel();
+    
+    public CalendarItemViewModel Day
+    {
+        get => _day;
+        set => SetAndRaise(DayProperty, ref _day, value);
+    }
 }

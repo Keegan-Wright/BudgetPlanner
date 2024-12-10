32e8853363e9c389272c5bea4a9dc7b42680bfbb
Keegan Wright <Keeganwright12@gmail.com>
2024-12-10 10:25:45 +0000

    Streamlines env setup

--- BudgetPlanner.Host.AppHost/Program.cs
+++ BudgetPlanner.Host.AppHost/Program.cs
@@ -4,7 +4,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 //ar password = builder.AddParameter("password", value:"Test", secret: false);
 
 
-
 var sentryConfig = builder.Configuration.GetSection("Sentry");
 var openBankingConfig = builder.Configuration.GetSection("OpenBanking");
 
@@ -15,21 +14,8 @@ var postgresDb = postgres.AddDatabase("budgetPlannerPostgresDb");
 
 
 var server = builder.AddProject<Projects.BudgetPlanner_Server>("BudgetPlannerServer")
-    
-    .WithEnvironment("SENTRY_DSN", sentryConfig["Dsn"])
-    .WithEnvironment("SENTRY_DEBUG", sentryConfig["Debug"])
-    .WithEnvironment("SENTRY_AUTO_SESSION_TRACKING", sentryConfig["AutoSessionTracking"])
-    .WithEnvironment("SENTRY_TRACES_SAMPLE_RATE", sentryConfig["TracesSampleRate"])
-    .WithEnvironment("SENTRY_PROFILES_SAMPLE_RATE", sentryConfig["ProfilesSampleRate"])
-    .WithEnvironment("SENTRY_RELEASE", sentryConfig["Release"])
-    .WithEnvironment("SENTRY_CAPTURE_FAILED_REQUESTS", sentryConfig["CaptureFailedRequests"])
-    
-    .WithEnvironment("OPEN_BANKING_TRUELAYER_BASE_AUTH_URL", openBankingConfig["TrueLayer:BaseAuthUrl"])
-    .WithEnvironment("OPEN_BANKING_TRUELAYER_BASE_DATA_URL", openBankingConfig["TrueLayer:BaseDataUrl"])
-    .WithEnvironment("OPEN_BANKING_TRUELAYER_AUTH_REDIRECT_URL", openBankingConfig["TrueLayer:AuthRedirectUrl"])
-    .WithEnvironment("OPEN_BANKING_TRUELAYER_CLIENT_ID", openBankingConfig["TrueLayer:ClientId"])
-    .WithEnvironment("OPEN_BANKING_TRUELAYER_CLIENT_SECRET", openBankingConfig["TrueLayer:ClientSecret"])
-    
+    .AddSentry(sentryConfig)
+    .AddOpenBanking(openBankingConfig)
     .WithReference(postgresDb)
     .WaitFor(postgresDb);
 
@@ -37,7 +23,6 @@ var server = builder.AddProject<Projects.BudgetPlanner_Server>("BudgetPlannerSer
 //var desktopClient = builder.AddProject<Projects.BudgetPlanner_Client_Desktop>("BudgetPlannerClientDesktop")
 //    .WithReference(server)
 //    .WaitFor(server);
-
 
 
 builder.Build().Run();
